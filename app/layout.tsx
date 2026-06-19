80b95eff348956479db175ceb5d9b84edd3477e8
Antony <afedryandi@gmail.com>
2026-06-19 10:25:47 +0700

    style: migrasi seluruh kode styles.css ke dalam globals.css utama

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
+import './globals.css';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
-import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Ginnva Shield Indonesia',
